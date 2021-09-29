6ca27bcaba41ea089356a898f5616aaac92c7c4d
dgupta23 <dgupta23@ncsu.edu>
2021-09-29 15:42:21 -0400

    Updated Create room look and feel

--- frontend/src/views/CreateRoom.tsx
+++ frontend/src/views/CreateRoom.tsx
@@ -12,10 +12,13 @@ export default function CreateRoom({ history }: RouteComponentProps) {
       <section className='header relative pt-16 items-center flex h-screen max-h-860-px'>
         <div className='container mx-auto items-center flex flex-wrap'>
           <div className='w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4'>
-            <div className="w-2/3">
-              <div>
-                <label className='create-room-text flex flex-col justify-center items-center'>
-                  Host Name
+            <div className="w-2/3 row flex flex-col justify-center items-center">
+              <span className='flex flex-col justify-center items-center mb-2 items-center text-4xl font-medium place-content-center mb-12'>
+                Create a personal room
+              </span>
+              <div className="row flex flex-col justify-center items-center">
+                <label className='w-full mb-1 text-2xl font-medium'>
+                  Host name
                 </label>
                 <input
                   id='host_name'
@@ -23,13 +26,13 @@ export default function CreateRoom({ history }: RouteComponentProps) {
                   onChange={(event) => {
                     setName(event.target.value)
                   }}
-                  className='create-room-input get-started text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-1 bg-lightBlue-500 active:bg-lightBlue-600 uppercase text-sm shadow hover:shadow-lg ease-linear transition-all duration-150'
+                  className='bg-lightBlue-500 text-white placeholder-white::placeholder rounded-lg' placeholder="Enter you name" style={{ color: '#BEE3F8' }}
                 ></input>
               </div>
-              <div className='row flex flex-col justify-center items-center'>
+              <div className='row flex flex-col justify-center items-center' >
                 <br />
-                <label className='create-room-text flex flex-col justify-center items-center'>
-                  Room Name
+                <label className='w-full mb-1 text-2xl font-medium'>
+                  Room name
                 </label>
                 <input
                   id='room_name'
@@ -37,7 +40,7 @@ export default function CreateRoom({ history }: RouteComponentProps) {
                   onChange={(event) => {
                     setTitle(event.target.value)
                   }}
-                  className='create-room-input get-started text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-1 bg-lightBlue-500 active:bg-lightBlue-600 uppercase text-sm shadow hover:shadow-lg ease-linear transition-all duration-150'
+                  className='rounded-lg text-white placeholder-white::placeholder bg-lightBlue-500' placeholder="Enter room name"
                 ></input>
               </div>
               <div className='mt-12'>
@@ -61,7 +64,7 @@ export default function CreateRoom({ history }: RouteComponentProps) {
             </div>
           </div>
         </div>
-{/* 
+        {/* 
         <img
           className='absolute top-0 b-auto right-0 pt-16 sm:w-6/12 -mt-48 sm:mt-0 w-10/12 max-h-860px'
           src={require('assets/img/pattern_react.png').default}
