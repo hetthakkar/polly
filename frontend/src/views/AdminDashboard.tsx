4554c6a24e51c42283b109e2716cbc702d654d18
divyankgupta23 <divyankgupta8@gmail.com>
2021-09-30 01:45:27 -0400

    Modified admin dashboard UI

--- frontend/src/views/AdminDashboard.tsx
+++ frontend/src/views/AdminDashboard.tsx
@@ -11,41 +11,51 @@ export default function AdminDashboard({ history }: RouteComponentProps) {
     useContext(AppContext)
 
   useEffect(() => {
-    (async function() {
-      const data = await fetchRoomAnalytics({roomId});
+    (async function () {
+      const data = await fetchRoomAnalytics({ roomId });
       console.log(data);
     })();
   });
 
   return (
     <>
-      <div>Hello {name}</div>
-      <div>Welcome to {title} your personal room</div>
+      <section className='header relative pt-16 items-center flex h-screen max-h-860-px'>
+        <div className='container mx-auto items-center flex flex-wrap'>
+          <div className='w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4'>
+            <div className="w-2/3 row flex flex-col justify-center items-center">
+              <span className='font-semibold flex flex-col justify-center items-center mb-2 items-center text-4xl font-medium place-content-center -mt-24'>
+                Hello {name},
+              </span>
+              <span className='font-semibold flex flex-col justify-center items-center items-center text-4xl font-medium place-content-center '>
+                Welcome to your personal room
+              </span>
 
-      {/* //TODO! List questions with stats */}
+              {/* //TODO! List questions with stats */}
 
-      <div
-        className='text-white text-center cursor-pointer'
-        style={{ background: '#4299E1', width: '80vw' }}
-        onClick={() => {
-          history.push('/create-questions');
-        }}
-      >
-        Create a question!
-      </div>
+              <div
+                className='mt-12 get-started text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-1 bg-lightBlue-500 active:bg-lightBlue-600 uppercase text-sm shadow hover:shadow-lg ease-linear transition-all duration-150'
+                onClick={() => {
+                  history.push('/create-questions');
+                }}
+              >
+                Create a question!
+              </div>
 
-      <div className="mt-10">
-        Share your personal room
-      </div>
-      <span>
-        <div className="flex flex-row">
-          <div>{process.env.REACT_APP_FRONT_END_BASE_URL + '/enter-room?code=' + roomKey}</div>
-          <div className="ml-3 cursor-pointer" onClick={() => {
-            navigator.clipboard.writeText(process.env.REACT_APP_FRONT_END_BASE_URL + '/enter-room?code=' + roomKey);
-          }} >Copy link url</div>
+              <div className="row flex flex-col justify-center items-center mt-10 w-full mb-1 text-3xl font-medium">
+                Share your personal room
+              </div>
+              <span>
+                <div className="flex flex-row">
+                  <div>{process.env.REACT_APP_FRONT_END_BASE_URL + '/enter-room?code=' + roomKey}</div>
+                  <div className="ml-3 cursor-pointer border-2 text-gray-400	" onClick={() => {
+                    navigator.clipboard.writeText(process.env.REACT_APP_FRONT_END_BASE_URL + '/enter-room?code=' + roomKey);
+                  }} >  Copy  </div>
+                </div>
+              </span>
+            </div>
+          </div>
         </div>
-      </span>
-
+      </section>
     </>
   )
 }
